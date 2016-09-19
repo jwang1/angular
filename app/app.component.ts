0053e1e7480656048ed4ede724a25b380f68802f
jwang1 <sigkdd@yahoo.com>
2016-09-19 00:59:53 -0400

    Routing added.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,26 +2,53 @@ import {Component} from 'angular2/core';
 
 import {HTTP_PROVIDERS} from 'angular2/http';
 import "rxjs/Rx";
+import {ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 
 import {ProductListComponent} from './products/product-list.component';
 import {ProductService} from './products/product.service';
+import {WelcomeComponent} from './home/welcome.component';
 
 
 @Component({
     selector: 'main-app',
+    // template: `
+    // <div><h2>{{pageTitle}}</h2>
+    //     <pm-products></pm-products>
+    // <div>
+    // `,
     template: `
-    <div><h2>{{pageTitle}}</h2>
-        <pm-products></pm-products>
     <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['Welcome']">Home</a></li>
+                    <li><a [routerLink]="['Products']">Product List</a></li>
+                </ul>
+            </div>  
+        </nav>
+    </div>
+    
+    <div class='container'>
+        <router-outlet></router-outlet>
+    </div>
     `,
     
-    directives: [ProductListComponent],
-    providers: [ProductService, HTTP_PROVIDERS]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ProductService, 
+                HTTP_PROVIDERS,
+                ROUTER_PROVIDERS]
     
 })
+@RouteConfig([
+    {path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true},
+    {path: '/products', name: 'Products', component: ProductListComponent}
+]) 
 export class AppComponent {
 
     pageTitle: string = 'Product Management.';
 
 }   
+
+
